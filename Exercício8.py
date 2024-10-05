38e323562588bbaa341efe3dcd0cde3a3b3cb08b
BRCMOURA <brcmoura@gmail.com>
2024-10-04 21:29:36 -0300

    Add files via upload

---
+++ Exercício8.py
@@ -0,0 +1,7 @@
+#Conversão de temperatura
+
+c = int(input("Digite a temperatura na escala Celsius: "))
+
+temperatura_fahrenheit = (9 * c + 160) / 5
+
+print("A temperatura na escala Fahrenheit é: ", temperatura_fahrenheit)
